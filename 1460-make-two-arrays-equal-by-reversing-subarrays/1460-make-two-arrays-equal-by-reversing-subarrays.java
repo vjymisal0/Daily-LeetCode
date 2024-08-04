36d11ff5e5a665408212003f14d3a199188f9697
Vijay Misal <misalvijay153@gmail.com>
2024-08-04 11:33:47 +0530

    Time: 4 ms (54.22%), Space: 44.2 MB (56.77%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.java
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.java
@@ -1,14 +1,17 @@
 class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
-        int[] cnt1 = new int[1001];
-        int[] ctn2 = new int[1001];
-        for (int v : target) {
-            ++cnt1[v];
-        }
-        for(int v: arr)
-        {
-            ++ctn2[v];
-        }
-        return Arrays.equals(cnt1, ctn2);
+        // int[] cnt1 = new int[1001];
+        // int[] ctn2 = new int[1001];
+        // for (int v : target) {
+        //     ++cnt1[v];
+        // }
+        // for(int v: arr)
+        // {
+        //     ++ctn2[v];
+        // }
+        // return Arrays.equals(cnt1, ctn2);
+         Arrays.sort(target);
+    Arrays.sort(arr);
+    return Arrays.equals(target, arr);
     }
 }
